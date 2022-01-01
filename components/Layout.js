057c182c9307d1e017d9b11d82ecfa2c411bd25f
nafllhh <naufalendwin7@gmail.com>
2022-01-01 10:56:22 +0700

    tweak

---
+++ components/Layout.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '/styles/Home.module.css'
+export default function Layout(props) {
+  return (
+    <div>
+        <Head>
+          <title>{props.title}</title>
+    </Head>
+    {props.children}
+
+      <footer className={styles.footer}>
+        <a
+  href =
+      "https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+  target = "_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={
+    16} />
+          </span>
+            &nbsp; and <font className={styles.next}>&nbsp;Next.js</font> 
+            <span>Copyright naufal</span>
+        </a>
+    <br/>
+      </footer>
+    </div>
+    
+  )
+}
